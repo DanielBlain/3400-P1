fbb2ad2b5437f3bf61c1efe7ff43b9e660b30339
Daniel Blain <dblain2@my.bcit.ca>
2024-07-23 17:46:30 -0700

    Add PageNotFound (forgot it first time) and useParam on PageSingle

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -6,17 +6,19 @@ import PageFavourites   from '../pages/PageFavourites'
 import PageAbout        from '../pages/PageAbout'
 import PageHelp         from '../pages/PageHelp'
 import PageLogin        from '../pages/PageLogin'
+import PageNotFound     from '../pages/PageNotFound'
 
 const AppRouter = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/'             element={<PageHome />}          />
+                <Route path='/'             element={<PageHome />}  exact   />
                 <Route path='/single/:id'   element={<PageSingle />}        />
                 <Route path='/favourites'   element={<PageFavourites />}    />
                 <Route path='/about'        element={<PageAbout />}         />
                 <Route path='/help'         element={<PageHelp />}          />
                 <Route path='/login'        element={<PageLogin />}         />
+                <Route path='*'             element={<PageNotFound />}      />
             </Routes>
         </BrowserRouter>
     )
