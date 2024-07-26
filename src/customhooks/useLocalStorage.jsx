9cefb6a6ae0bf939e85106b23b1d75865e0066c9
Daniel Blain <dblain2@my.bcit.ca>
2024-07-26 04:33:36 -0700

    Dispatch manager complete (somehow), not extensively tested

--- src/customhooks/useLocalStorage.jsx
+++ src/customhooks/useLocalStorage.jsx
@@ -2,23 +2,6 @@ import { useEffect } from 'react'
 
 /*****
  * 
- *      key: (string), the name of the key in localStorage
- *      value: (any), state data we wish to persist through
- *          the use of localStorage
- *      setValue: (func) a setter func for value
- * 
- * returns: [ value with storage attached under key,
- * 
- *              setter func for value that also
- *              updates storage when changed,
- * 
- *              func that retrieves from key in 
- *              localStorage ]
- * 
- * */
-
-/*****
- * 
  * ---- written by Daniel J Blain, 24 July 2024
  * 
  * useLocalStorage is a React custom hook that takes in a
--- src/pages/PageHome.jsx
+++ src/pages/PageHome.jsx
@@ -1,65 +1,15 @@
-import { useContext, useReducer } from 'react'
+import { useContext } from 'react'
 import { CustomContext } from '../components/CustomContext'
-// import useReducerManager from '../customhooks/useReducerManager'
-
-const initialTodos = [
-    {
-      id: 1,
-      title: "Todo 1",
-      complete: false,
-    },
-    {
-      id: 2,
-      title: "Todo 2",
-      complete: false,
-    },
-  ];
-
-
-  const reducer = (state, action) => {
-    switch (action.type) {
-      case "COMPLETE":
-        return state.map((todo) => {
-            return (todo.id === action.id) ?
-                { ...todo, complete: !todo.complete } 
-                : todo;
-          }
-        );
-      default:
-        return state;
-    }
-  };
 
 const PageHome = () => {
-
+    
     const [ message, setMessage ] = useContext(CustomContext)
-    const [ todos, dispatch ] = useReducer(reducer, initialTodos)
-
-    const handleComplete = (todo) => {
-        dispatch({ type: "COMPLETE", id: todo.id });
-      };
 
     return (
         <>
             <div>
                 <div className='testing'>
                     {/*<!-- TESTING -->*/}
-
-                    <button onClick={() => {console.log('damn')}}>
-                        Console Logger
-                    </button>
-                    {todos.map((todo) => (
-                        <div key={todo.id}>
-                        <label>
-                            <input
-                            type="checkbox"
-                            checked={todo.complete}
-                            onChange={() => handleComplete(todo)}
-                            />
-                            {todo.title}
-                        </label>
-                        </div>
-                    ))}
 
                     <button onClick={() => {
                         localStorage.setItem('beeswax', 'potato')
