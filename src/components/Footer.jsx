d75ac0fbe4865ef063d1059e5797454311f327f2
Daniel Blain <dblain2@my.bcit.ca>
2024-07-27 06:28:59 -0700

    Fiddling with footer, probably waste of time

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,26 @@
 import DanLogo from '/DanLogo-x1024.png' // From public folder
-import { author } from '../global/global'
-import { getYear } from '../utilities/utilities'
+import { author }               from '../config/config'
+import { authorQuote }          from '../config/config'
+import { socialMediaAccounts }  from '../config/config'
+import { getYear }              from '../utilities/utilities'
 
 const Footer = () => (
     <footer>
-        <div className='footer-right'>socials</div>
-        <div className='footer-left'>
-            <img
-                className="DanLogo"
-                src={DanLogo}
-                alt="Logo for Dan J Blain"
-            />
-            {getYear()}  &copy;  {author}
+        <div className='socialMediaPanel'>
+            Testing
+        </div>
+        <div>
+            <span className='copyrightTag'>
+                <span>
+                    <a href='https://www.danjblain.dev' target="_blank" rel="noreferrer">
+                            <img src={DanLogo} alt='Logo of Dan J Blain' />
+                            {getYear()} &copy; {author} &nbsp;&nbsp;&nbsp;
+                    </a>
+                    <em>
+                        {authorQuote}
+                    </em>
+                </span>
+            </span>
         </div>
     </footer>
 )
