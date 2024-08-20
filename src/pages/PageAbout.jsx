fcf74a21d3558ab56813a8492df3c637d2163435
Daniel Blain <dblain2@my.bcit.ca>
2024-08-19 22:47:36 -0700

    About page styled as much as I want to do. Done

--- src/pages/PageAbout.jsx
+++ src/pages/PageAbout.jsx
@@ -35,47 +35,100 @@ const PageAbout = () => {
             <h2>About</h2>
             <section className='pageAbout'>
 
-                {/** Disclaimer(s) */}
-                <article className='appAdvertPanel'>
-                    <p>
-                        <b>NOTE:</b> <strong>{appName}</strong> is meant strictly as an
-                        educational project, and therefore should <em>not</em> be
-                        used for any professional or business purposes. If you
-                        choose to do so, it is at your own risk, and I take utterly
-                        no responsibility!
-                    </p>
-                </article>
+                <div>
 
-                {/** TMDB gratitude */}
-                <article>
-                    <p>
-                        This product uses the TMDB API but is not endorsed or
-                        certified by TMDB.
-                    </p>
-                    <div className='tmdbLogo'>
-                        <img
-                            src={ imageFolder + '/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg' }
-                            alt="Logo for TMDB (The Movie Database)"
-                        />
+                    <div className='gratitude'>
+                        {/** TMDB gratitude */}
+                        <article>
+                            <p>
+                                This product uses the TMDB API but is not endorsed or
+                                certified by TMDB.
+                            </p>
+                            <span>
+                                <img
+                                    src={ imageFolder + '/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg' }
+                                    alt="Logo for TMDB (The Movie Database)"
+                                />
+                            </span>
+                        </article>
+
+                        {/** BCIT gratitude */}
+                        <article>
+                            <p>
+                                <strong>{appName}</strong> is an educational project built by
+                                Dan J. Blain as part of a class that teaches students
+                                to use the Javascript React framework.
+                            </p>
+                            <span>
+                                <img
+                                    src={ imageFolder + '/bcit.png' }
+                                    alt="Logo for BCIT (British Columbia Institute of Technology)"
+                                />
+                            </span>
+                            <p>
+                                The React class was part of the Frontend Web Development program
+                                offered by the brilliant people at BCIT.
+                            </p>
+                            <p>
+                                Thank you for my education!
+                            </p>
+                        </article>
                     </div>
-                </article>
 
-                {/** BCIT gratitude */}
-                <article>
-                    <p>
-                        <strong>{appName}</strong> is an educational project built by
-                        Dan J. Blain as part of a class that teaches students
-                        to use the Javascript React framework.
-                    </p>
-                    <img src={ imageFolder + '/bcit.png' } alt="Logo for BCIT (British Columbia Institute of Technology)" />
-                    <p>
-                        The React class was part of the Frontend Web Development program
-                        offered by the brilliant people at BCIT.
-                    </p>
-                    <p>
-                        Thank you for my education!
-                    </p>
-                </article>
+                    {/** Disclaimer(s) */}
+                    <article className='disclaimer'>
+                        <p>
+                            <b>NOTE:</b> <strong>{appName}</strong> is meant strictly as an
+                            educational project, and therefore should <em>not</em> be
+                            used for any professional or business purposes. If you
+                            choose to do so, it is at your own risk, and I take utterly
+                            no responsibility!
+                        </p>
+                    </article>
+
+                    {/* Icons & other gratitude */}
+                    <article className='codeAttributionList'>
+                        <p>
+                            Small bit(s) of things/code from:
+                        </p>
+                        <ul>
+                            <li>
+                                <a
+                                    className='isUrl'
+                                    href='https://css-tricks.com/snippets/css/prevent-long-urls-from-breaking-out-of-container/'
+                                >
+                                    https://css-tricks.com/snippets/css/prevent-long-urls-from-breaking-out-of-container/
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    className='isUrl'
+                                    href='https://css-tricks.com/how-to-create-neon-text-with-css/'
+                                >
+                                    https://css-tricks.com/how-to-create-neon-text-with-css/
+                                </a>
+                            </li>
+                            <li>
+                                Television static gif from
+                                <a
+                                    className='isUrl'
+                                    href='https://cliply.co/clip/static-noise/'
+                                >
+                                    https://cliply.co/clip/static-noise/
+                                </a>
+                            </li>
+                        </ul>
+                        <p>
+                            Social media icons provided by <a
+                                className='isUrl'
+                                href='https://iconpacks.net/?utm_source=link-attribution&utm_content=6532'
+                            >
+                                Iconpacks
+                            </a>
+                        </p>
+                    </article>
+
+                </div>
 
                 {/** Product advertising */}
                 <article className='appAdvertPanel'>
@@ -116,47 +169,6 @@ const PageAbout = () => {
                         what&apos;s <Link to='/'>Now Playing</Link> to get started!</p>
                 </article>
 
-                {/* Icons & other gratitude */}
-                <article>
-                    <p>
-                        Small bit(s) of things/code from:
-                    </p>
-                    <ul className='codeAttributionList'>
-                        <li>
-                            <a
-                                className='isUrl'
-                                href='https://css-tricks.com/snippets/css/prevent-long-urls-from-breaking-out-of-container/'
-                            >
-                                https://css-tricks.com/snippets/css/prevent-long-urls-from-breaking-out-of-container/
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                className='isUrl'
-                                href='https://css-tricks.com/how-to-create-neon-text-with-css/'
-                            >
-                                https://css-tricks.com/how-to-create-neon-text-with-css/
-                            </a>
-                        </li>
-                        <li>
-                            Television static gif from
-                            <a
-                                className='isUrl'
-                                href='https://cliply.co/clip/static-noise/'
-                            >
-                                https://cliply.co/clip/static-noise/
-                            </a>
-                        </li>
-                    </ul>
-                    <p>
-                        Social media icons provided by <a
-                            className='isUrl'
-                            href='https://iconpacks.net/?utm_source=link-attribution&utm_content=6532'
-                        >
-                            Iconpacks
-                        </a>
-                    </p>
-                </article>
             </section>
         </section>
     )
